0000b1f99b681566d65bd38cbc9381362a57d526
Vikram Baliga <vbaliga87@gmail.com>
2026-05-15 10:48:44 -0700

    clean up rendering process and standardize packages

--- build_all.R
+++ build_all.R
@@ -1,4 +1,8 @@
-rmarkdown::clean_site()
-rmarkdown::clean_site("RLabs")
+rm(list = ls())
+
+options(width = 75)
+
+rmarkdown::clean_site(preview = FALSE)
+rmarkdown::clean_site("RLabs",preview = FALSE)
 rmarkdown::render_site(".", encoding = "UTF-8")
 rmarkdown::render_site("RLabs", encoding = "UTF-8")
