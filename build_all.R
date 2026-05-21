b7a846843d04151c261fc3e37d4f0e67b2265fb9
Vikram Baliga <vbaliga87@gmail.com>
2026-05-21 14:58:41 -0700

    update Sampling lab and make it lab 3

--- build_all.R
+++ build_all.R
@@ -1,8 +1,9 @@
-rm(list = ls())
-
-options(width = 75)
-
-rmarkdown::clean_site(preview = FALSE)
-rmarkdown::clean_site("RLabs",preview = FALSE)
-rmarkdown::render_site(".", encoding = "UTF-8")
-rmarkdown::render_site("RLabs", encoding = "UTF-8")
+callr::r(function() {
+  options(width = 75)
+  
+  rmarkdown::clean_site(".", preview = FALSE)
+  rmarkdown::clean_site("RLabs", preview = FALSE)
+  
+  rmarkdown::render_site(".", encoding = "UTF-8")
+  rmarkdown::render_site("RLabs", encoding = "UTF-8")
+})
